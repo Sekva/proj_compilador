f1856289a74f25db32625175af01eb5043ec4ac9
Matheus Machado <matheusmachado708@gmail.com>
2019-09-05 14:04:09 -0300

    Só deixando bonito

--- compilador/src/analisador_lexico/token.rs
+++ compilador/src/analisador_lexico/token.rs
@@ -2,14 +2,14 @@ use crate::analisador_lexico::tipo_token::*;
 use std::fmt;
 
 pub struct Token {
-    token : Tipo_Token,
-    lexema : String,
-    literal : String,
-    linha : usize,
-    valor_bool : Option<bool>,
-    valor_char : Option<char>,
-    valor_str : Option<String>,
-    valor_int : Option<i128>,
+    token       : Tipo_Token,
+    lexema      : String,
+    literal     : String,
+    linha       : usize,
+    valor_bool  : Option<bool>,
+    valor_char  : Option<char>,
+    valor_str   : Option<String>,
+    valor_int   : Option<i128>,
     valor_float : Option<f64>,
 }
 
@@ -27,8 +27,6 @@ impl Token {
 
             let temp_literal = literal.to_string();
 
-
-
             match tipo {
 
                 Tipo_Token::TRUE  => { opt_bool  = Some(true) },
@@ -41,9 +39,7 @@ impl Token {
                 _ => {}
             }
 
-
         }
-
 
         Token {
             token       : tipo,
@@ -58,38 +54,40 @@ impl Token {
         }
     }
 
-    pub fn lexema(&self) -> String {
-        self.lexema.to_string()
-    }
-    pub fn literal(&self) -> String {
-        self.literal.to_string()
-    }
-    pub fn token(&self) -> Tipo_Token {
-        self.token
-    }
-    pub fn linha(&self) -> usize {
-        self.linha
-    }
+    pub fn lexema  (&self) -> String     { self.lexema.to_string()  }
+    pub fn literal (&self) -> String     { self.literal.to_string() }
+    pub fn token   (&self) -> Tipo_Token { self.token               }
+    pub fn linha   (&self) -> usize      { self.linha               }
 
-    pub fn valor_bool(&self)  -> Option<bool>   { self.valor_bool  }
-    pub fn valor_char(&self)  -> Option<char>   { self.valor_char  }
-    pub fn valor_str(&self)   -> Option<String> { self.valor_str.clone() }
-    pub fn valor_int(&self)   -> Option<i128>   { self.valor_int   }
-    pub fn valor_float(&self) -> Option<f64>    { self.valor_float }
+    pub fn valor_bool  (&self) -> Option<bool>   { self.valor_bool        }
+    pub fn valor_char  (&self) -> Option<char>   { self.valor_char        }
+    pub fn valor_str   (&self) -> Option<String> { self.valor_str.clone() }
+    pub fn valor_int   (&self) -> Option<i128>   { self.valor_int         }
+    pub fn valor_float (&self) -> Option<f64>    { self.valor_float       }
 }
 
 impl PartialEq for Token {
+
     fn eq(&self, other: &Self) -> bool {
+
         self.token == other.token
         && self.lexema == other.lexema
         && self.literal == other.literal
         && self.linha == other.linha
+
     }
+
 }
 
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,"Tipo : {}; Lexema: {}; Literal: {}; Linha: {}", self.token(), self.lexema(), self.literal(), self.linha())
+
+        write!(f,"Tipo : {}; Lexema: {}; Literal: {}; Linha: {}",
+            self.token(),
+            self.lexema(),
+            self.literal(),
+            self.linha()
+        )
     }
 }
 
